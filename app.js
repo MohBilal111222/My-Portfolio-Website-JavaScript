db49b94e143d92e409ea5efc065ede9f158b1a84
Bilal <mohammadbilal91112@gmail.com>
2024-09-19 04:50:42 +0500

    working on ir

---
+++ app.js
@@ -0,0 +1,18 @@
+    // var typed = new Typed('#element', {
+    //   strings: ['Mern Stack Developer ' , 'Deep Learning Engineer' , 'Web Designer' , 'Mobile Application Developer'],
+    //   typeSpeed: 50,
+      
+    // });
+
+
+
+    var typed = new Typed('#element', {
+        strings: ['Mern Stack Developer', 'Deep Learning Engineer', 'Web Designer', 'Mobile Application Developer'],
+        typeSpeed: 50,      // Speed of typing
+        backSpeed: 30,      // Speed of backspacing
+        loop: true,         // Loop the animation
+        smartBackspace: true,  // Only backspace to the point of difference
+        backDelay: 1500,    // Delay before backspacing
+        startDelay: 500     // Delay before typing starts
+      });
+      
